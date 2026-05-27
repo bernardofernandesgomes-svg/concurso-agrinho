9c46b1a82faeda0b16287face417f41ea02918ae
bernardofernandesgomes-svg <bernardo.fernandes.gomes@escola.pr.gov.br>
2026-05-27 11:42:59 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("botaoMensagem");
+
+botao.addEventListener("click", () => {
+  alert("🌱 A sustentabilidade na agricultura é essencial para proteger o planeta e garantir alimento para o futuro!");
+});
